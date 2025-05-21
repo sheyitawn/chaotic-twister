9678089bedba5a327216f8d33648d985c1442e47
sheyitawn <randombuzzer234@gmail.com>
2025-05-21 20:25:10 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Play from './pages/Play';
-import Calibrate from './pages/Calibrate';
+import Home from './pages/Home/Home';
+import Play from './pages/Play/Play';
+import Calibrate from './pages/Calibrate/Calibrate';
+import { PlayerProvider } from './context/PlayerContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/play" element={<Play />} />
-        <Route path="/calibrate" element={<Calibrate />} />
-      </Routes>
-    </Router>
+    <PlayerProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/calibrate" element={<Calibrate />} />
+        </Routes>
+      </Router>
+    </PlayerProvider>
   );
 }
 
